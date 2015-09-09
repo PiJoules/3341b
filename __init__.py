c18b7bb858dfd1290d7d645569359e9f62d14335
pijoules <lchan1994@yahoo.com>
2015-09-08 23:02:13 -0400

    gotta increase that github score

--- __init__.py
+++ __init__.py
@@ -4,14 +4,19 @@ vendor.add('lib')
 
 
 # Import the Flask Framework
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, current_app
 app = Flask(__name__)
 
 
+import json
+
+
 # Root directory
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-	return render_template("index.html")
+	with open("people.json") as f:
+		people = json.load(f)
+	return render_template("index.html", people=people)
 
 
 if __name__ == '__main__':
--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
+#!/usr/bin
+
 # Clear anything already in lib/ except the README
 shopt -s extglob
 rm -rf lib/!(README.md)
 
 # Install python dependencies
 pip install -r requirements.txt -t lib/
-
-# Clear anything in the private/ directory
-rm -rf private/!(README.md|__init__.py)
